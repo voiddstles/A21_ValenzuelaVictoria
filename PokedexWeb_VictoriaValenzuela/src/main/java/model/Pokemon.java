b0cb80dc827fbdfba14919a95dab96d182b25dfd
voiddstles <victoria.vzls24@gmail.com>
2025-10-31 18:46:50 -0700

    proyecto pokedex victoria

---
+++ PokedexWeb_VictoriaValenzuela/src/main/java/model/Pokemon.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author victoria
+ */
+public class Pokemon implements Serializable{
+    private String nombre;
+    private int numero;
+    private String tipo;
+    private String imageUrl;
+    
+    public Pokemon(){
+        
+    }
+    
+    public Pokemon(String nombre, int numero, String tipo, String imageUrl){
+        this.nombre = nombre;
+        this.numero = numero;
+        this.tipo = tipo;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+    
+    
+}
